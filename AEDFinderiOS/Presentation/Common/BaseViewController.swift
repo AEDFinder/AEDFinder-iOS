5ab7935e719027e8a2c6c0be68b60921b0d47f8b
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-11-20 23:01:42 +0900

    [ADD] Base View Controller

--- AEDFinderiOS/Presentation/Common/BaseViewController.swift
+++ AEDFinderiOS/Presentation/Common/BaseViewController.swift
@@ -5,4 +5,26 @@
 //  Created by 이동희 on 2022/11/20.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class BaseViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAttribute()
+        setLayout()
+    }
+    
+    func setAttribute() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    
+}
