c33381339b711522a67c127eaca149a9adb43123
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-11-20 23:03:45 +0900

    [ADD] Home View Controller structure

--- AEDFinderiOS/Presentation/HomeScene/ViewController/HomeViewController.swift
+++ AEDFinderiOS/Presentation/HomeScene/ViewController/HomeViewController.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+final class HomeViewController: BaseViewController {
+    
+    weak var coordinator: HomeCoordinator?
+    private var viewModel: HomeViewModel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+    static func create(
+        with viewModel: HomeViewModel,
+        _ coordinator: HomeCoordinator
+    ) -> HomeViewController {
+        let vc = HomeViewController()
+        vc.viewModel = viewModel
+        vc.coordinator = coordinator
+        return vc
+    }
+}
