744e39853be2bdfb6e24f44173c3bf06ab0cb414
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-11-22 20:43:56 +0900

    [ADD] Location Permission Manager

--- AEDFinderiOS/Network/LocationPermissionManager.swift
+++ AEDFinderiOS/Network/LocationPermissionManager.swift
@@ -5,4 +5,43 @@
 //  Created by 이동희 on 2022/11/22.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import RxCoreLocation
+import CoreLocation
+
+class LocationPermissionManager {
+    static let shared = LocationPermissionManager()
+    private let disposeBag = DisposeBag()
+    
+    let locationSubject = BehaviorSubject<CLLocation?>(value: nil)
+    private let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = kCLDistanceFilterNone
+        return manager
+    }()
+    
+    private init() {
+        locationManager.rx.didUpdateLocations
+            .compactMap(\.locations.last?)
+            .bind(onNext: self.locationSubject.onNext(_:))
+            .disposed(by: disposeBag)
+        locationManager.stopUpdatingLocation() //이미 권한을 허용한 경우의 케이스
+    }
+    
+    func requestLocation() -> Observable<CLAuthorizationStatus> {
+        return Observable<CLAuthorizationStatus>
+            .deferred { [weak self] in
+                guard let self = self else { return .empty() }
+                self.locationManager.requestWhenInUseAuthorization()
+                return self.locationManager.rx.didChangeAuthorization
+                    .map { $1 }
+                    .filter { $0 != .notDetermined }
+                    .do(onNext: { _ in self.locationManager.startUpdatingLocation()
+                        self.locationManager.stopUpdatingLocation()
+                    })
+                    .take(1)
+            }
+    }
+}
