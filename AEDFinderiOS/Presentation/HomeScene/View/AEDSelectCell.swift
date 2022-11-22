c3afb7c49cecc234528543b840c998777ce1f2ff
Lee, Donghee <67154245+mechanicdong@users.noreply.github.com>
2022-11-22 20:42:41 +0900

    [ADD] AED List CollectionView Cell

--- AEDFinderiOS/Presentation/HomeScene/View/AEDSelectCell.swift
+++ AEDFinderiOS/Presentation/HomeScene/View/AEDSelectCell.swift
@@ -5,4 +5,43 @@
 //  Created by 이동희 on 2022/11/21.
 //
 
-import Foundation
+import UIKit
+import Then
+import SnapKit
+
+ class AEDSelectCell: UICollectionViewCell {
+    static let identifier = "AEDSelectCell"
+    
+    private let title = UILabel().then {
+        $0.font = .SD_Gothic(.light, size: 14)
+        $0.textColor = .red
+        $0.textAlignment = .left
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setLayout()
+    }
+     
+     required init?(coder: NSCoder) {
+         super.init(coder: coder)
+         self.setLayout()
+     }
+     
+    private func setLayout() {
+        self.contentView.backgroundColor = .yellow
+        [title]
+            .forEach {
+                self.contentView.addSubview($0)
+            }
+        
+        title.snp.makeConstraints {
+            $0.leading.top.bottom.equalToSuperview()
+        }
+    }
+    
+    func update(with aed: HomeInfo) {
+        title.text = "\(aed.latitude)"
+    }
+    
+}
